c91d41675f688d96346994d462dee321840b0eff
gabriel-brito <mfkrgabriel@gmail.com>
2017-08-19 14:51:04 -0300

    Fixing sintaxe

--- beginner-html-site-styled-gh-pages/scripts/main.js
+++ beginner-html-site-styled-gh-pages/scripts/main.js
@@ -16,7 +16,7 @@ let myButton = document.querySelector('button');
 
 function setUserName(){
 	let myName = prompt('Please enter your name');
-	localStorage = setItem('name', myName);
+	localStorage.setItem('name', myName);
 	myHeading.textContent = `Mozilla is cool, ${myName}!`; 
 }
 
